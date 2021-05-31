17062bce137b6d333311b41ed799478c1a86c2d1
Leonardo Pereira <leonardo.psico@outlook.com.br>
2021-05-31 18:15:18 -0300

    iniciamos por aqui

--- script.js
+++ script.js
@@ -1,14 +1,42 @@
-const button = document.getElementById("teste");
-button.addEventListener("click", function () {
+// const button = document.getElementById("teste");
+// button.addEventListener("click", function () {
 //    temCoisa()
 
-});
+// });
+
+// // // if (tower1 .childElementCount > 0) { //pega a contagem de elementos filhos
+// // //     count = tower1.childElementCount; //se houver variavel, guarda na variavel qual a contagem;
+// // //     alert(count)
+// // //     }
+// // // }
+// let body;
 
 
+// function criarDiv() {
+//     let tower = document.createElement("div")
+//     return tower;
+// }
 
-// // if (tower1 .childElementCount > 0) { //pega a contagem de elementos filhos
-// //     count = tower1.childElementCount; //se houver variavel, guarda na variavel qual a contagem;
-// //     alert(count)
-// //     }
-// // }
+// let tower1 = new Torre()
+// let tower2 = new Torre()
+// let tower3 = new Torre()
+
+// function Torre() {
+//     this.tower = criarDiv();
+//     this.tower.style.width = "25%";
+//     this.tower.style.height = "50px"
+//     this.tower.style.border = "solid green"
+//     this.tower.style.marginLeft =  "4%"
+//     this.tower.style.borderWidth = "2%"
+//     this.tower.style.float = "left"
+// }
+
+// function iniciar() {
+//     body = document.getElementsByName("body")[0];
+//     body.appendChild(tower1.Torre)
+//     body.appendChild(tower2.Torre)
+//     body.appendChild(tower3.Torre)
+// }
+
+// window.addEventListener("load", iniciar, false)
 
