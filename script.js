1ba9fc00b05f00d543f0d664bb124132f87a8948
Leonardo Pereira <leonardo.psico@outlook.com.br>
2021-05-31 15:43:02 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const button = document.getElementById("teste");
+button.addEventListener("click", function () {
+//    temCoisa()
+
+});
+
+
+
+// // if (tower1 .childElementCount > 0) { //pega a contagem de elementos filhos
+// //     count = tower1.childElementCount; //se houver variavel, guarda na variavel qual a contagem;
+// //     alert(count)
+// //     }
+// // }
+
